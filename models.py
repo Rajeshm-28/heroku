135b58832cde9ae1a71bc0dd8c7f8c3b44644faf
Rajeshm-28 <rajeshm.28@gmail.com>
2020-05-25 14:49:04 +0530

    final

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_path = "postgresql://DELL@localhost:5432/heroku_sample"
+database_path = os.environ['DATABASE_URL']
 
 
 db = SQLAlchemy()
---
+++ setup.sh
@@ -0,0 +1,1 @@
+DATABASE_URL=postgresql://DELL@localhost:5432/heroku_sample
